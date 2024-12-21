cca94f159552612640d7be05aab76b2362eafae2
Saketh2714 <sakethchowdhary167@gmail.com>
2024-12-21 13:09:59 +0530

    2 file

--- ArrayDemo1.java
+++ ArrayDemo1.java
@@ -7,15 +7,6 @@ public class ArrayDemo1 {
 	public static void main(String[] args) {
 		// left rotation one move
 				int[]  num = {60,70,80,90,100};
-				// 5
-				// 0 --4
-				// 4--3
-				//  100 60 70 80 90
-				// 70 80 90 100 60--- left
-				// num[4]--num[3]
-				//num[3]---num[2]
-				// num[2]---num[1]
-				// num[1]---num[0]
 				System.out.println("Before rotation");
 				for(int i=0;i<num.length;i++) {
 					System.out.print(num[i]+" ");
